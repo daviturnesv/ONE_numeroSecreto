8c1d4f5c2bdc663cab1340780763ee711b4fcbb5
daviturnesv <davi.turnesv@gmail.com>
2025-01-30 14:58:31 -0300

    Loops e tentativas

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ alert("Seja bem vindo ao Jogo do Número Secreto! Vai se aprumando ai");
 let numeroSecreto = Math.floor(Math.random() * 100) + 1;
 console.log(numeroSecreto);
 
-let guess = prompt("Escolhe um numero entre 1 e 100, rapidin");
+let guess = null;
+let rodada = 1;
 
-if (numeroSecreto == guess){
-    alert(`Boa guri(a), acertasse o numero ${numeroSecreto}`);
-    console.log("Acertou!");
-} else{
+while (numeroSecreto != guess){
+    guess = prompt(`Rodada ${rodada}\nEscolhe um numero entre 1 e 100, rapidin`);
+    console.log(`Rodada ${rodada}`);
     if (numeroSecreto > guess){
         alert(`Puts, o numero secreto é maior que ${guess}`);
-    }else{
+        console.log("Errou!!");
+    }else if(numeroSecreto < guess){
         alert(`Bah, o numero secreto é menor que ${guess}`);
+        console.log("Errou!!");
+    } else if (guess == null){
+        alert("Ai tu comprica, tens que digitar algo");
+        console.log("Jogador não digitou");
     }
-    console.log("Errou!!");
+    rodada += 1;
 }
+alert(`Boa guri(a), acertasse o numero secreto (${numeroSecreto}) em ${rodada - 1} tentativa(s)`);
+console.log("Acertou!");
